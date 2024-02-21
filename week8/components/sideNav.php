9b63b326dcbdab57a1325b24f050ff0369ebc618
Chairivir <70614188+Jecolambo@users.noreply.github.com>
2024-02-21 09:08:20 +0800

    week8 - revamp

---
+++ week8/components/sideNav.php
@@ -0,0 +1,117 @@
+<!-- Side Nav -->
+<nav class="side-nav-expanded bg-dark col-lg-1 order-lg-1 fixed-bottom p-3 p-lg-0" id="side-nav">
+    
+    <!-- Side Nav Button Carousel -->
+    <section class="btn-carousel p-0 pb-3 d-flex flex-lg-column gap-3 justify-content-start justify-content-md-evenly gap-lg-0">
+        
+        <!-- Profile Button -->
+        <a href="#" class="btn btn-outline-light p-5">
+        
+            <!-- Profile Button Icon -->
+            <i class=" bi bi-person-circle fs-1"></i>
+            <!-- Profile Button Icon -->
+            
+            <!-- Profile Button Title -->
+            <span class="fs-5">
+                Profile
+            </span>
+            <!-- Profile Button Title -->
+            
+        </a>
+        <!-- Profile Button -->
+
+        <!-- PokéDopt Button -->
+        <a href="#" class="btn btn-outline-light p-5">
+
+            <!-- PokéDopt Button Icon -->
+            <img src="../assets/img/favicon.ico" alt="" class="img mb-1">
+            <!-- PokéDopt Button Icon -->
+            
+            <!-- PokéDopt Button Title -->
+            <span class="fs-5">
+                PokéDopt
+            </span>
+            <!-- PokéDopt Button Title -->
+
+        </a>
+        <!-- PokéDopt Button -->
+        
+        <!-- PokéTrade Button -->
+        <a href="#" class="btn btn-outline-light p-5">
+
+            <!-- PokéTrade Button Icon -->
+            <img src="../assets/img/poketrade.ico" alt="" class="img mb-1">
+            <!-- PokéTrade Button Icon -->
+            
+            <!-- PokéTrade Button Title -->
+            <span class="fs-5">
+                PokéTrade
+            </span>
+            <!-- PokéTrade Button Title -->
+
+        </a>
+        <!-- PokéTrade Button -->
+
+        <!-- PokéPals Button -->
+        <a href="#" class="btn btn-outline-light p-5">
+
+            <!-- PokéPals Button Icon -->
+            <img src="../assets/img/pokepals.ico" alt="" class="img mb-1">
+            <!-- PokéPals Button Icon -->
+            
+            <!-- PokéPals Button Title -->
+            <span class="fs-5">
+                PokéPals
+            </span>
+            <!-- PokéPals Button Title -->
+
+        </a>
+        <!-- PokéPals Button -->
+
+        <!-- PokÉvents Button -->
+        <a href="#" class="btn btn-outline-light p-5">
+
+            <!-- PokÉvents Button Icon -->
+             <i class="bi bi-calendar-event fs-1"></i>
+            <!-- PokÉvents Button Icon -->
+            
+            <!-- PokÉvents Button Title -->
+            <span class="fs-5">
+                PokÉvents
+            </span>
+            <!-- PokÉvents Button Title -->
+
+        </a>
+        <!-- PokÉvents Button -->
+
+        <!-- PokéChat Button -->
+        <a href="#" class="btn btn-outline-light p-5">
+
+            <!-- PokéChat Button Icon -->
+            <i class="bi bi-chat-dots-fill fs-1"></i>
+            <!-- PokéChat Button Icon -->
+            
+            <!-- PokéChat Button Title -->
+            <span class="fs-5">
+                PokéChat
+            </span>
+            <!-- PokéChat Button Title -->
+
+        </a>
+        <!-- PokéChat Button -->
+
+        <!-- Side Nav Toggler -->
+        <button class="btn btn-outline-light fixed-bottom d-none d-lg-block" id="side-nav-toggler">
+
+            <!-- Side Nav Toggler Icon  -->
+            <i class="bi bi-box-arrow-in-down-left fs-1" id="side-nav-toggler-icon"></i>
+            <!-- Side Nav Toggler Icon  -->
+
+        </button>
+        <!-- Side Side Nav Toggler -->
+        
+    </section>
+    <!-- Side Nav Button Carousel -->
+
+</nav>
+<!-- Side Nav -->
---
+++ week8/pages/pokedopt.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    
+        <!-- Meta -->
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Meta -->
+        
+        <!-- Bootstrap CDN -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <!-- Bootstrap CDN -->
+        
+        <!-- Bootstrap Icon CDN -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+        <!-- Bootstrap Icon CDN -->
+        
+        <!-- Website Header -->
+        <link rel="icon" type="image/x-icon" href="../assets/img/favicon.ico">
+        <title>PokéDopt</title>
+        <!-- Website Header -->
+        
+        <!-- Components CSS -->
+        <link rel="stylesheet" href="../styles/components.css">
+        <!-- Components CSS -->
+    
+    </head>
+    <body class="bg-secondary">
+        
+        <!-- Page -->
+        <div class="container-fluid">
+
+            <!-- Page Row -->
+            <div class="row">
+
+                <!-- Primary Section -->
+                <section class="primary col-md-11 order-md-2 p-0 m-0">
+                    Owo
+                </section>
+                <!-- Primary Section -->
+                
+                <!-- Side Nav -->
+                <?php include '../components/sideNav.php' ?>
+                <!-- Side Nav -->
+
+            </div>
+            <!-- Page Row -->
+
+        </div>
+        <!-- Page -->
+    
+        <!-- Bootstrap JS CDN -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <!-- Bootstrap JS CDN -->
+        
+        <!-- JQuery CDN -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <!-- JQuery CDN -->
+
+        <!-- Components JavaScript -->
+        <script src="../javascripts/components.js"></script>
+        <!-- Components JavaScript -->
+
+    </body>
+</html>
