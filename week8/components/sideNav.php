5a22a0498b26b333204542f0dd3ff9cde5870b8b
AshieLoche <asherstar1001@gmail.com>
2024-02-22 18:13:49 +0800

    week8 - revamp

--- week8/components/sideNav.php
+++ week8/components/sideNav.php
@@ -1,117 +1,116 @@
-<!-- Side Nav -->
-<nav class="navbar side-nav-expanded bg-dark col-lg-1 order-lg-1 fixed-bottom p-3 p-lg-0" id="side-nav">
+
+<nav class="navbar side-nav-expanded bg-dark col-md-2 order-md-1 fixed-bottom p-3 p-md-0 d-flex align-items-md-start" id="side-nav">
+
+<!-- Side Nav Button Carousel -->
+<section class="btn-carousel p-0 pb-3 pb-md-0 d-flex flex-md-column gap-3 gap-md-0 justify-content-start">
+
+    <!-- Profile Button -->
+    <a href="#" class="btn btn-outline-light p-5">
+
+        <!-- Profile Button Icon -->
+        <i class=" bi bi-person-circle fs-1"></i>
+        <!-- Profile Button Icon -->
+
+        <!-- Profile Button Title -->
+        <span class="fs-5">
+            Profile
+        </span>
+        <!-- Profile Button Title -->
+
+    </a>
+    <!-- Profile Button -->
+
+    <!-- PokéDopt Button -->
+    <a href="#" class="btn btn-outline-light p-5">
+
+        <!-- PokéDopt Button Icon -->
+        <img src="../assets/img/icons/pokedopt.ico" alt="" class="img mb-1">
+        <!-- PokéDopt Button Icon -->
+        
+        <!-- PokéDopt Button Title -->
+        <span class="fs-5">
+            PokéDopt
+        </span>
+        <!-- PokéDopt Button Title -->
+
+    </a>
+    <!-- PokéDopt Button -->
+
+    <!-- PokéTrade Button -->
+    <a href="#" class="btn btn-outline-light p-5">
+
+        <!-- PokéTrade Button Icon -->
+        <img src="../assets/img/icons/poketrade.ico" alt="" class="img mb-1">
+        <!-- PokéTrade Button Icon -->
+        
+        <!-- PokéTrade Button Title -->
+        <span class="fs-5">
+            PokéTrade
+        </span>
+        <!-- PokéTrade Button Title -->
+
+    </a>
+    <!-- PokéTrade Button -->
+
+    <!-- PokéPals Button -->
+    <a href="#" class="btn btn-outline-light p-5">
+
+        <!-- PokéPals Button Icon -->
+        <img src="../assets/img/icons/pokepals.ico" alt="" class="img mb-1">
+        <!-- PokéPals Button Icon -->
+        
+        <!-- PokéPals Button Title -->
+        <span class="fs-5">
+            PokéPals
+        </span>
+        <!-- PokéPals Button Title -->
+
+    </a>
+    <!-- PokéPals Button -->
+
+    <!-- PokÉvents Button -->
+    <a href="#" class="btn btn-outline-light p-5">
+
+        <!-- PokÉvents Button Icon -->
+         <i class="bi bi-calendar-event fs-1"></i>
+        <!-- PokÉvents Button Icon -->
+        
+        <!-- PokÉvents Button Title -->
+        <span class="fs-5">
+            PokÉvents
+        </span>
+        <!-- PokÉvents Button Title -->
+
+    </a>
+    <!-- PokÉvents Button -->
+
+    <!-- PokéChat Button -->
+    <a href="#" class="btn btn-outline-light p-5">
+
+        <!-- PokéChat Button Icon -->
+        <i class="bi bi-chat-dots-fill fs-1"></i>
+        <!-- PokéChat Button Icon -->
+        
+        <!-- PokéChat Button Title -->
+        <span class="fs-5">
+            PokéChat
+        </span>
+        <!-- PokéChat Button Title -->
+
+    </a>
+    <!-- PokéChat Button -->
+
+    <!-- Side Nav Toggler -->
+    <button class="btn btn-outline-light fixed-bottom d-none d-md-block" id="side-nav-toggler">
+
+        <!-- Side Nav Toggler Icon  -->
+        <i class="bi bi-box-arrow-in-down-left fs-1" id="side-nav-toggler-icon"></i>
+        <!-- Side Nav Toggler Icon  -->
+
+    </button>
+    <!-- Side Side Nav Toggler -->
     
-    <!-- Side Nav Button Carousel -->
-    <section class="col-12 btn-carousel p-0 pb-3 pb-md-0 d-flex flex-lg-column gap-3 gap-lg-0 justify-content-start justify-content-md-evenly">
-        
-        <!-- Profile Button -->
-        <a href="#" class="btn btn-outline-light p-5">
-        
-            <!-- Profile Button Icon -->
-            <i class=" bi bi-person-circle fs-1"></i>
-            <!-- Profile Button Icon -->
-            
-            <!-- Profile Button Title -->
-            <span class="fs-5">
-                Profile
-            </span>
-            <!-- Profile Button Title -->
-            
-        </a>
-        <!-- Profile Button -->
-
-        <!-- PokéDopt Button -->
-        <a href="#" class="btn btn-outline-light p-5">
-
-            <!-- PokéDopt Button Icon -->
-            <img src="../assets/img/favicon.ico" alt="" class="img mb-1">
-            <!-- PokéDopt Button Icon -->
-            
-            <!-- PokéDopt Button Title -->
-            <span class="fs-5">
-                PokéDopt
-            </span>
-            <!-- PokéDopt Button Title -->
-
-        </a>
-        <!-- PokéDopt Button -->
-        
-        <!-- PokéTrade Button -->
-        <a href="#" class="btn btn-outline-light p-5">
-
-            <!-- PokéTrade Button Icon -->
-            <img src="../assets/img/poketrade.ico" alt="" class="img mb-1">
-            <!-- PokéTrade Button Icon -->
-            
-            <!-- PokéTrade Button Title -->
-            <span class="fs-5">
-                PokéTrade
-            </span>
-            <!-- PokéTrade Button Title -->
-
-        </a>
-        <!-- PokéTrade Button -->
-
-        <!-- PokéPals Button -->
-        <a href="#" class="btn btn-outline-light p-5">
-
-            <!-- PokéPals Button Icon -->
-            <img src="../assets/img/pokepals.ico" alt="" class="img mb-1">
-            <!-- PokéPals Button Icon -->
-            
-            <!-- PokéPals Button Title -->
-            <span class="fs-5">
-                PokéPals
-            </span>
-            <!-- PokéPals Button Title -->
-
-        </a>
-        <!-- PokéPals Button -->
-
-        <!-- PokÉvents Button -->
-        <a href="#" class="btn btn-outline-light p-5">
-
-            <!-- PokÉvents Button Icon -->
-             <i class="bi bi-calendar-event fs-1"></i>
-            <!-- PokÉvents Button Icon -->
-            
-            <!-- PokÉvents Button Title -->
-            <span class="fs-5">
-                PokÉvents
-            </span>
-            <!-- PokÉvents Button Title -->
-
-        </a>
-        <!-- PokÉvents Button -->
-
-        <!-- PokéChat Button -->
-        <a href="#" class="btn btn-outline-light p-5">
-
-            <!-- PokéChat Button Icon -->
-            <i class="bi bi-chat-dots-fill fs-1"></i>
-            <!-- PokéChat Button Icon -->
-            
-            <!-- PokéChat Button Title -->
-            <span class="fs-5">
-                PokéChat
-            </span>
-            <!-- PokéChat Button Title -->
-
-        </a>
-        <!-- PokéChat Button -->
-
-        <!-- Side Nav Toggler -->
-        <button class="btn btn-outline-light fixed-bottom d-none d-lg-block" id="side-nav-toggler">
-
-            <!-- Side Nav Toggler Icon  -->
-            <i class="bi bi-box-arrow-in-down-left fs-1" id="side-nav-toggler-icon"></i>
-            <!-- Side Nav Toggler Icon  -->
-
-        </button>
-        <!-- Side Side Nav Toggler -->
-        
-    </section>
-    <!-- Side Nav Button Carousel -->
+</section>
+<!-- Side Nav Button Carousel -->
 
 </nav>
-<!-- Side Nav -->
--- week8/pages/pokedopt.php
+++ week8/pages/pokedopt.php
@@ -13,7 +13,8 @@
             pokemon.name,
             species.species,
             type1.type as type1,
-            type2.type as type2
+            type2.type as type2,
+            pokemon.description
         FROM pokedopt.pokemon as pokemon
         INNER JOIN pokedopt.species as species ON pokemon.species_id = species.id
         INNER JOIN pokedopt.type as type1 ON species.type1_id = type1.id
@@ -31,14 +32,6 @@
 
             // Free results From Memory
             mysqli_free_result($result);
-
-            // foreach ($pokemon as $poke) {
-            //     echo '<img src="data:image/jpeg;base64,' . base64_encode($poke['img']) . '" style="width: 500px; height: 500p;" />';
-            //     echo "<p> {$poke['name']} </p>";
-            //     echo "<p> {$poke['species']} </p>";
-            //     echo "<p> {$poke['type1']} </p>";
-            //     echo "<p> {$poke['type2']} </p>";
-            // }
         }
 
         // Close Connection
@@ -50,9 +43,35 @@
 
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+    
+        <!-- Meta -->
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Meta -->
+        
+        <!-- Bootstrap CDN -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <!-- Bootstrap CDN -->
+        
+        <!-- Bootstrap Icon CDN -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+        <!-- Bootstrap Icon CDN -->
+        
+        <!-- Website Header -->
+        <link rel="icon" type="image/x-icon" href="assets/img/icons/pokedopt.ico">
+        <title>PokéDopt</title>
+        <!-- Website Header -->
+        
+        <!-- Components CSS -->
+        <link rel="stylesheet" href="styles/components.css">
+        <!-- Components CSS -->
+    
+    </head>
     <body class="bg-secondary">
         
-        <!-- Page -->
+        <!-- Page Container -->
         <div class="container-fluid">
 
             <!-- Page Row -->
@@ -62,59 +81,83 @@
                 <section class="primary col-md-10 order-md-2 p-0">
                     
                     <!-- Header Nav -->
-                <?php include '../components/headerNav.php' ?>
+                <?php require '../components/headerNav.php' ?>
                     <!-- Header Nav -->
 
                     <!-- Main -->
-                    <main class="bg-secondary" id="pokedoptables">
+                    <main class="bg-secondary">
 
                         <!-- Main Container -->
                         <div class="container-fluid bg-info">
                             
                             <!-- Main Row -->
                             <div class="row gap-5 p-4 py-sm-5 justify-content-center bg-danger">
-                                
-                                <!-- Pokémon Card 1 -->
-                                <article class="card bg-dark text-light border-light rounded-5 col-sm-10 col-md-8 col-lg-6 col-xl-4 col-xxl-3 p-0">
-                                    
-                                    <!-- Pokémon Card 1 Image -->
-                                    <img src="assets/img/chandelure.jpg" alt="Chandelure" class="card-img-top img rounded-5">
-                                    <!-- Pokémon Card 1 Image -->
 
-                                    <!-- Pokémon Card 1 Body -->
-                                    <section class="card-body pt-3 px-4 pb-5">
+                                <?php foreach ($pokemon as $poke) { ?>
+
+                                    <!-- Pokémon Card -->
+                                    <article class="card bg-dark text-light border-dark rounded-5 col-sm-10 col-md-8 col-lg-6 col-xl-4 col-xxl-3 p-0">
                                         
-                                        <!-- Pokémon Card 1 Title Container -->
-                                        <div class="border-bottom pb-2 container fluid">
+                                        <!-- Pokémon Card Image -->
+                                        <img src="data:image/jpeg;base64,<?php echo base64_encode($poke['img']); ?>" alt="<?php echo $poke['species']; ?>" class="card-img-top img rounded-top-5 border-bottom border-bottom">
+                                        <!-- Pokémon Card Image -->
 
-                                            <!-- Pokémon Card 1 Title Row -->
-                                            <div class="row">
+                                        <!-- Pokémon Card Body -->
+                                        <section class="card-body pt-3 px-4 pb-5">
+                                            
+                                            <!-- Pokémon Card Header Container -->
+                                            <div class="border-bottom pb-2 mb-3 container fluid">
 
-                                                <!-- Pokémon Card 1 Title -->
-                                                <a href="#" class="card-header display-3 col-10 col-lg-9 p-0 bg-info text-decoration-none">
-                                                    Amier
-                                                </a>
-                                                <!-- Pokémon Card 1 Title -->
+                                                <!-- Pokémon Card Header Row -->
+                                                <div class="row">
 
-                                                <!-- Pokémon Card 1 Button Container -->
+                                                    <!-- Pokémon Card Header -->
+                                                    <a href="#" class="card-header display-3 col-10 col-lg-9 p-0 text-decoration-none border-0">
+                                                        <?php echo $poke['name']; ?>
+                                                    </a>
+                                                    <!-- Pokémon Card Header -->
+
+                                                    <!-- Pokémon Card Button Span -->
+                                                    <span class="col-2 col-lg-3 d-flex justify-content-end align-items-center p-0">
+                                                        
+                                                        <!-- Pokémon Card Button -->
+                                                        <button class="btn btn-outline-light border-0 p-0 px-2">
+
+                                                            <!-- Pokémon Card Button Title -->
+                                                            <i class="card-header p-0 bi bi-suit-heart display-3"></i>
+                                                            <!-- Pokémon Card Button Title -->
+
+                                                        </button>
+                                                        <!-- Pokémon Card Button -->
+
+                                                    </span>
+                                                    <!-- Pokémon Card Button Span -->
+
+                                                </div>
+                                                <!-- Pokémon Card Header Row -->
 
                                             </div>
-                                            <!-- Pokémon Card 1 Title Row -->
+                                            <!-- Pokémon Card Header Container -->
+                                            
+                                            <!-- Pokémon Card Title -->
+                                            <section class="card-title display-4 fs-2">
+                                                <?php echo $poke['species']; ?>
+                                            </section>
+                                            <!-- Pokémon Card Title -->
+                                            
+                                            <!-- Pokémon Card Description -->
+                                            <section class="card-text display-5 fs-4 lh-sm">
+                                                <?php echo $poke['description']; ?>
+                                            </section>
+                                            <!-- Pokémon Card Description -->
 
-                                        </div>
-                                        <!-- Pokémon Card 1 Title Container -->
+                                        </section>
+                                        <!-- Pokémon Card Body -->
 
-                                    </section>
-                                    <!-- Pokémon Card 1 Body -->
+                                    </article>
+                                    <!-- Pokémon Card -->
 
-                                </article>
-                                <!-- Pokémon Card 1 -->
-                                
-                                <!-- Pokémon Card 2 -->
-                                <article class="card bg-dark text-light border-light rounded-5 col-sm-10 col-md-8 col-lg-6 col-xl-4 col-xxl-3 p-0">
-                                    OwO
-                                </article>
-                                <!-- Pokémon Card 2 -->
+                                <?php } ?>
 
                             </div>
                             <!-- Main Row -->
@@ -129,14 +172,14 @@
                 <!-- Primary Section -->
                 
                 <!-- Side Nav -->
-                <?php include '../components/sideNav.php' ?>
+                <?php require '../components/sideNav.php' ?>
                 <!-- Side Nav -->
 
             </div>
             <!-- Page Row -->
 
         </div>
-        <!-- Page -->
+        <!-- Page Container -->
     
         <!-- Bootstrap JS CDN -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
@@ -147,7 +190,7 @@
         <!-- JQuery CDN -->
 
         <!-- Components JavaScript -->
-        <script src="../javascripts/components.js"></script>
+        <script src="javascripts/components.js"></script>
         <!-- Components JavaScript -->
 
     </body>
